ece3858f03a069694b0c0f97a872dd2d289fbf4e
AnthCoding <anth.coding@users.noreply.github.com>
2021-09-20 13:49:52 -0400

    Context

---
+++ src/store/auth-context.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLogInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLogInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+  };
+  const loginHandler = () => {
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
